dd6e78b46c9839463105c5096e36d63d79dc386e
Julick Mellah <74454432+Melliche@users.noreply.github.com>
2023-09-22 09:10:42 +0200

    commit

--- migrations/Version20230921162904.php
+++ migrations/Version20230921162904.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230921101134 extends AbstractMigration
+final class Version20230921162904 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -26,7 +26,7 @@ final class Version20230921101134 extends AbstractMigration
         $this->addSql('CREATE TABLE note (id INT AUTO_INCREMENT NOT NULL, note DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE question (id INT AUTO_INCREMENT NOT NULL, test_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_B6F7494E1E5D0459 (test_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE test (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, complete_courses LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A251E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
         $this->addSql('ALTER TABLE chapter ADD CONSTRAINT FK_F981B52E591CC992 FOREIGN KEY (course_id) REFERENCES course (id)');
--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,20 +2,81 @@
 
 namespace App\Controller;
 
+use App\Form\TestFormType;
+use App\Repository\AnswerRepository;
+use App\Repository\ChapterRepository;
+use App\Repository\CourseRepository;
+use App\Repository\QuestionRepository;
+use App\Repository\TestRepository;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\DTO\TestAnswerDTO;
+use App\Form\TestAnswerFormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
 
 class CourseController extends AbstractController
 {
-    #[Route('/course', name: 'app_course')]
-    public function index(): Response
+    #[Route('/course/{id}', name: 'app_course', methods: ['GET', 'POST'])]
+    public function index(Request $request, CourseRepository $courseRepository, ChapterRepository $chapterRepository, QuestionRepository $questionRepository, AnswerRepository $answerRepository, TestRepository $testRepository, ManagerRegistry $doctrine, $id): Response
     {
+
+        $course = $courseRepository->find($id);
+        $chapters = $course->getChapters();
+        $test = $course->getTest();
+        $questions = $test->getQuestions();
+
+        $count = 0;
+        $error_message = null;
+
+        if ($request->isMethod('POST')) {
+            $formData = $request->request->all();
+            foreach ($formData as $answerId) {
+                $ans = $answerRepository->find($answerId);
+                $trueAnswer = $ans->isValid();
+                if ($trueAnswer == true) {
+                    $count++;
+                }
+                if ($count == 3) {
+                    $user = $this->getUser();
+                    $completeCourses = $user->getCompleteCourses();
+                    if ($completeCourses === null) {
+                        $completeCourses = [];
+                    }
+
+                    $courseId = $course->getId();
+                    if (!in_array($courseId, $completeCourses)) {
+                        $completeCourses[] = $courseId;
+                        $user->setCompleteCourses($completeCourses);
+
+                        $entityManager = $doctrine->getManager();
+                        $entityManager->persist($user);
+                        $entityManager->flush();
+                    }
+
+
+                    dump($user->getEmail());
+                }
+            }
+            if ($count !== 3) {
+                $error_message = "Tu as une erreur";
+            }
+            dump($count);
+        }
+
         return $this->render('course/index.html.twig', [
             'controller_name' => 'CourseController',
+            'course' => $course,
+            'chapters' => $chapters,
+            'questions' => $questions,
+            'id' => $id,
+            'error_message' => $error_message
+
         ]);
     }
-
-
-    // FAIRE FIXTURES TEST ET QUESTIONS ET LIER TEST ET COURS
 }
